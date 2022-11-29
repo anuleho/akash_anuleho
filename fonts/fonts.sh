836de26619b48b18ef483ab2c82196f8325f60e7
anuleho <anuleho.biswas@niser.ac.in>
2022-11-29 21:51:42 +0530

    initial commit

---
+++ fonts/fonts.sh
@@ -0,0 +1,7 @@
+wget https://anulehoastro.my.canva.site/04-298-3985-2092-76-209-1092-4095-info-mollysrestaurant-com/fonts/e2de09eac0386e92b44a70cb071e6ef7.woff2
+wget https://anulehoastro.my.canva.site/04-298-3985-2092-76-209-1092-4095-info-mollysrestaurant-com/fonts/4377ff44aa925d2cc65596b765a8219b.woff2
+wget https://anulehoastro.my.canva.site/04-298-3985-2092-76-209-1092-4095-info-mollysrestaurant-com/fonts/e0f9b30091328904135f7ea0923c802b.woff2
+wget https://anulehoastro.my.canva.site/04-298-3985-2092-76-209-1092-4095-info-mollysrestaurant-com/fonts/58b393ddd1cfd8653a34e37082411b9d.woff2
+wget https://anulehoastro.my.canva.site/04-298-3985-2092-76-209-1092-4095-info-mollysrestaurant-com/fonts/254a21cb559da08565d4a9c0c0b84a36.woff2
+wget https://anulehoastro.my.canva.site/04-298-3985-2092-76-209-1092-4095-info-mollysrestaurant-com/fonts/99ddc70a8ffa326837e6983f9aed0872.woff2
+wget https://anulehoastro.my.canva.site/04-298-3985-2092-76-209-1092-4095-info-mollysrestaurant-com/fonts/fd616ed261fdc236ca4db06cde59f2b2.woff2
